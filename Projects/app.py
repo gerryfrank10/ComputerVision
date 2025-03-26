fa0a3a52e45a7e7e93e81c5a4d7a158f774a6c46
gerald <amasi.gerald@icloud>
2025-03-26 11:50:22 +0000

    added flask app and keep sketching project

---
+++ Projects/app.py
@@ -0,0 +1,22 @@
+from flask import Flask, request, render_template, send_file
+from PIL import Image
+import io
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/convert', methods=['POST'])
+def convert():
+    file = request.files['image']
+    format = request.form['format']
+    image = Image.open(file)
+    img_io = io.BytesIO()
+    image.save(img_io, format.upper())
+    img_io.seek(0)
+    return send_file(img_io, mimetype=f'image/{format}')
+
+if __name__ == '__main__':
+    app.run(debug=True)
